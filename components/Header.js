1ca66dab570fc1b3a709c1a4adf941369223afc0
AYTSistemas13 <edu.richarte@gmail.com>
2021-12-23 14:42:25 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,8 @@ function Header() {
                 className="object-contain"
                 src="https://links.papareact.com/ua6"
                 width={200}
-                height={100} />
+                height={100} 
+                alt="" />
         </header>
     )
 }
